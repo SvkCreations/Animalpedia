634cb00d7d923463b783d0497d29d03c5b830f89
SvkCreations <95845696+SvkCreations@users.noreply.github.com>
2021-12-19 13:14:16 +0530

    Add files via upload

---
+++ animals.js
@@ -0,0 +1,6 @@
+function mouseOver1(x){
+    x.style.cssText="padding-left:10px; transition-duration:.5s";
+}
+function mouseOut1(x){
+    x.style.cssText="padding-left:0px; transition-duration:.5s";
+}
